77c36d252dcf7709e5e22c09255925e5e9409132
Mushthak <mail.musthak@gmail.com>
2024-07-15 00:34:49 +0530

    website added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:developer';
-import 'dart:io';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:micons/icons.dart';
 import 'package:micons/micons.dart';
 
@@ -12,95 +9,143 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MICons Explore Icons',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'MICons Example'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Map<String, File> files = {};
-  Map<String, String> uploadedUrls = {};
+  TextEditingController searchController = TextEditingController();
+  List<String> filteredIconList = [];
+  bool showFilledIcons = false;
 
-  void imagePicker() async {
-    ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-    if (image != null) {
-      files["image"] = File(image.path);
-      MIconsUploader.uploadFiles(
-        files: files,
-        apiKey: "API_KEY",
-        folder: "FOLDER_NAME",
-      ).listen(
-        (progress) {
-          log("Progress for ${progress.fileKey} is ${progress.progress * 100}%");
-          if (progress.url != null) {
-            uploadedUrls[progress.fileKey] = progress.url!;
-          }
-          if (progress.urls != null) {
-            uploadedUrls = progress.urls!;
-          }
-        },
-        onDone: () {
-          log("All files uploaded. URLs:");
-          uploadedUrls.forEach((key, url) {
-            log("File key: $key, URL: $url");
-          });
-        },
-        onError: (error) {
-          log('Error in upload stream: $error');
-        },
-      );
-    }
+  @override
+  void initState() {
+    super.initState();
+    iconList.sort();
+    filteredIconList = iconList;
+  }
+
+  void _filterIcons(String query) {
+    final filtered = iconList
+        .where((icon) => icon.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    setState(() {
+      filteredIconList = filtered;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigoAccent.withOpacity(.1),
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MIcons(
-              icon: MiCons.badgeCheck,
-              color: Colors.black,
-              size: 80,
-            ),
-            const SizedBox(height: 40),
-            const Text("Upload Files Demo"),
-            TextButton(
-              onPressed: () {
-                imagePicker();
-              },
-              child: MIcons(
-                icon: MiCons.icloud,
-                color: Colors.black,
-                size: 40,
+        title: const Text(
+          'MICons Explore Icons',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent,
+        actions: [
+          Row(
+            children: [
+              Text(
+                showFilledIcons ? 'Filled' : 'Outlined',
+                style: const TextStyle(color: Colors.white),
               ),
+              const SizedBox(width: 5),
+              CupertinoSwitch(
+                thumbColor: Colors.indigoAccent,
+                trackColor: Colors.white,
+                activeColor: Colors.white.withOpacity(.7),
+                value: showFilledIcons,
+                onChanged: (value) {
+                  setState(() {
+                    showFilledIcons = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          const SizedBox(width: 10),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              onChanged: _filterIcons,
+              decoration: InputDecoration(
+                hintText: 'Search icons...',
+                hintStyle: const TextStyle(color: Colors.white),
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.2),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                ),
+                prefixIcon: const Icon(Icons.search, color: Colors.white),
+              ),
+              style: const TextStyle(color: Colors.white),
             ),
-          ],
+          ),
+        ),
+      ),
+      body: GridView.extent(
+        padding: const EdgeInsets.all(10),
+        maxCrossAxisExtent: 150,
+        children: List.generate(
+          filteredIconList.length,
+          (index) {
+            final icon = filteredIconList[index];
+            return Container(
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MIcons(
+                    icon: icon,
+                    filled: showFilledIcons,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    icon.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
   }
 }
+
+
